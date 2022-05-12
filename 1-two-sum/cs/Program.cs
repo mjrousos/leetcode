d1ff6b6e7eb1bdf2c4d2b03c82090b4f7295dc02
Mike Rousos <Mike@MJRousos.com>
2022-05-12 17:33:51 -0400

    Add a C# solution for two-sum

---
+++ 1-two-sum/cs/Program.cs
@@ -0,0 +1,23 @@
+﻿// https://leetcode.com/problems/two-sum/
+
+var solver = new Solution();
+var solution = solver.TwoSum(new [] {2, 7, 11, 15}, 9);
+Console.WriteLine($"{solution[0]}, {solution[1]}");
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        var goalIndexes = new Dictionary<int, short>();
+        goalIndexes[nums[0]] = 0;
+        for (var i = 1; i < nums.Length; i++)
+        {
+            var goal = target - nums[i];
+            if (goalIndexes.ContainsKey(goal))
+            {
+                return new [] {i, goalIndexes[goal]};
+            }
+            goalIndexes[nums[i]] = (short)i;
+        }
+
+        throw new ArgumentException("No valid solution");
+    }
+}
